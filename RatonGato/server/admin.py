becb17a4cee9362a68c19a6efa0f4269245938a5
espaguetissecos <espaguetissintomate@gmail.com>
2017-04-21 01:43:34 +0200

    RatonGato

--- RatonGato/server/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from server.models import User, Game, Move
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Game)
-admin.site.register(Move)
